87c3c46c374eb02dd07c5ef56bc56ee96a54e28b
Jacob Haslehurst <jacob@haslehurst.net>
2014-02-27 14:48:51 +1100

    adding test running script

---
+++ runtests.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys
+
+from django.conf import settings
+from django.core.management import execute_from_command_line
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+            }
+        },
+        USE_TZ=True,
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'south',
+            'generic_follow',
+            'tests',
+        ),
+        TEST_RUNNER='django_nose.NoseTestSuiteRunner',
+    )
+
+
+def runtests():
+    argv = sys.argv[:1] + ['test', 'tests']
+    execute_from_command_line(argv)
+
+
+if __name__ == '__main__':
+    runtests()

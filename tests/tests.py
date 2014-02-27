87c13507a11a041665254d3c10b88a83ca0e4715
Jacob Haslehurst <jacob@haslehurst.net>
2014-02-27 16:35:53 +1100

    adding tests for calling un/follow multiple times

--- tests/tests.py
+++ tests/tests.py
@@ -20,22 +20,38 @@ class GenericFollowModelTests(TestCase):
         self.assertEqual(follow.user, self.user)
         self.assertEqual(follow.target, self.band)
 
+    def test_calling_follow_multiple_times_doesnt_create_multiple_follows(self):
+        self.user.follow(self.band)
+        self.assertEqual(1, Follow.objects.count())
+
+        self.user.follow(self.band)
+        self.assertEqual(1, Follow.objects.count())
+
     def test_user_can_unfollow_successfully(self):
         self.user.follow(self.band)
-
         self.assertEqual(1, Follow.objects.count())
 
         self.user.unfollow(self.band)
+        self.assertEqual(0, Follow.objects.count())
 
+    def test_calling_unfollow_multiple_times_doesnt_cause_errors(self):
+        self.user.follow(self.band)
+        self.assertEqual(1, Follow.objects.count())
+
+        self.user.unfollow(self.band)
+        self.assertEqual(0, Follow.objects.count())
+
+        self.user.unfollow(self.band)
+        self.user.unfollow(self.band)
+        self.user.unfollow(self.band)
+        self.user.unfollow(self.band)
         self.assertEqual(0, Follow.objects.count())
 
     def test_is_following_returns_correct_result(self):
         self.assertFalse(self.user.is_following(self.band))
 
         self.user.follow(self.band)
-
         self.assertTrue(self.user.is_following(self.band))
 
         self.user.unfollow(self.band)
-
         self.assertFalse(self.user.is_following(self.band))

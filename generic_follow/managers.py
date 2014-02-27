2c7b99c6df35ea25878d1ae0c0a52ed028d59793
Justin Arulnathan <justin@gizmag.com>
2014-02-27 16:28:14 +1100

    added manager create_batch() method

--- generic_follow/managers.py
+++ generic_follow/managers.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.db.models.query import QuerySet
+from django.contrib.contenttypes.models import ContentType
+from django.db.models.loading import get_model
 
 
 class FollowQuerySet(QuerySet):
@@ -14,3 +16,17 @@ class FollowManager(models.Manager):
         if attr.startswith("_"):
             raise AttributeError
         return getattr(self.get_queryset(), attr, *args)
+
+    def create_batch(self, users, target):
+        target_content_type = ContentType.objects.get_for_model(target)
+        Follow = get_model('generic_follow', 'Follow')
+        follows = list()
+        for user in users:
+            list.append(
+                Follow(
+                    user=user,
+                    target_content_type=target_content_type,
+                    target_id=target.pk
+                )
+            )
+        self.objects.bulk_create(follows)

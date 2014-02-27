7858dea277da4596eef4c12da95a60816cbb23d4
Jacob Haslehurst <jacob@haslehurst.net>
2014-02-27 16:08:33 +1100

    adding failing test for User.follow

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from generic_follow.models import Follow
+from .models import Band
 
 
 class GenericFollowModelTests(TestCase):
-    pass
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='john',
+            password='password'
+        )
+        self.band = Band.objects.create(name='Foals')
+
+    def test_user_can_follow_band(self):
+        self.user.follow(band)
+
+        follow = Follow.objects.all()[0]
+
+        self.assertEqual(follow.user, self.user)
+        self.assertEqual(follow.target, self.band)

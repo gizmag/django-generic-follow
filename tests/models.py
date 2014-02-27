6813dcb9c3b07f02a2f7f2626c584bfa3aa02064
Jacob Haslehurst <jacob@haslehurst.net>
2014-02-27 16:59:17 +1100

    added Photographer tests model

--- tests/models.py
+++ tests/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Band(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Photographer(models.Model):
+    name = models.CharField(max_length=255)

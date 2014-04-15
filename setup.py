0c8ab72e88d905e99579f7215d07e3cc3bdc2f88
Kate Seamer <kate@gizmag.com>
2014-04-15 13:56:14 +1000

    bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-generic-follow',
-    version='0.1.0',
+    version='0.2.0',
     description='Generic follow system for Django',
     author='Gizmag',
     author_email='tech@gizmag.com',

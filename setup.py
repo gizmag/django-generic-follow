93c06ddfe41244802cef7c6f44129a94e5207b3e
Justin Arulnathan <justin@gizmag.com>
2015-11-26 17:33:56 +1100

    make it compatible with previous django versions

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-generic-follow',
-    version='0.4.1',
+    version='0.4.2',
     description='Generic follow system for Django',
     author='Gizmag',
     author_email='tech@gizmag.com',
     url='https://github.com/gizmag/django-generic-follow',
     packages=find_packages(exclude=['tests']),
-    install_requires=['django>=1.8']
+    install_requires=['django']
 )

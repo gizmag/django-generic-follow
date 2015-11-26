18c8355b939d00fdf9ce6bdc18e51b3de02d8857
Justin Arulnathan <justin@gizmag.com>
2015-11-26 17:41:42 +1100

    install south if required

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@
 
 from setuptools import setup, find_packages
 
+REQUIRES = ['django']
+try:
+    import django
+    if django.VERSION[:2] < (1, 7):
+        REQUIRES.append("South")
+except ImportError:
+    pass
+
 setup(
     name='django-generic-follow',
     version='0.4.2',
@@ -10,5 +18,5 @@ setup(
     author_email='tech@gizmag.com',
     url='https://github.com/gizmag/django-generic-follow',
     packages=find_packages(exclude=['tests']),
-    install_requires=['django']
+    install_requires=REQUIRES
 )
